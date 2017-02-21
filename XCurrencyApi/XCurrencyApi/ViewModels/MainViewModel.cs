807c19ecec8fd10b6babfe597c44aa6bd94d29d8
David Zambrano <davazamb10@outlook.com>
2017-02-21 12:28:11 -0400

    MainViewModel. Incluide, Attributes, Properties, Constructors.

--- XCurrencyApi/XCurrencyApi/ViewModels/MainViewModel.cs
+++ XCurrencyApi/XCurrencyApi/ViewModels/MainViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XCurrencyApi.Classes;
 
 namespace XCurrencyApi.ViewModels
 {
@@ -11,11 +13,123 @@ namespace XCurrencyApi.ViewModels
     {
         #region Attributes
 
+        private ExchangeRates exchangeRates;
+
+        private decimal amount;
+
+        private double sourceRate;
+
+        private double targetRate;
+
+        private bool isRunning;
+
+        private bool isEnabled;
+
+        private string message;
+
+        #endregion
+
+
+        #region Properties
+
+        public ObservableCollection<Rate> Rates { get; set; }
+
+        public decimal Amount
+        {
+            set
+            {
+                if (amount != value)
+                {
+                    amount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Amount"));
+                }
+            }
+            get
+            {
+                return amount;
+            }
+        }
+
+        public double SourceRate
+        {
+            set
+            {
+                if (sourceRate != value)
+                {
+                    sourceRate = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SourceRate"));
+                }
+            }
+            get
+            {
+                return sourceRate;
+            }
+        }
+
+        public double TargetRate
+        {
+            set
+            {
+                if (targetRate != value)
+                {
+                    targetRate = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TargetRate"));
+                }
+            }
+            get
+            {
+                return targetRate;
+            }
+        }
+
+        public bool IsRunning
+        {
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+                }
+            }
+            get
+            {
+                return isRunning;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+                }
+            }
+            get
+            {
+                return isEnabled;
+            }
+        }
 
         #endregion
 
         #region Events
-        public event PropertyChangedEventHandler PropertyChanged; 
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
+
+
+        #region Constructors
+
+        public MainViewModel()
+        //{
+            Rates = new ObservableCollection<Rate>();
+            IsEnabled = false;
+            GetRates();
+        }      
+        #endregion
+
     }
 }
