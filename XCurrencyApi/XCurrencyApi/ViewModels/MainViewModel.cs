b2cc1e512ee7ace047162114122c1d1c2686d74c
David Zambrano <davazamb10@outlook.com>
2017-02-21 12:13:26 -0400

    INotifyPropertyChanged in MainviewModel

--- XCurrencyApi/XCurrencyApi/ViewModels/MainViewModel.cs
+++ XCurrencyApi/XCurrencyApi/ViewModels/MainViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XCurrencyApi.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Attributes
 
+
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged; 
+        #endregion
     }
 }
